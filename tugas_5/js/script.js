dab464f1db8cff4871fe17dc234796de1257453f
HMNTR <kurniaputra.irham@gmail.com>
2025-03-11 05:39:25 +0700

    Add files via upload

---
+++ tugas_5/js/script.js
@@ -0,0 +1,56 @@
+let notes = [];
+
+
+function loadData() {
+    const data = localStorage.getItem('notes')
+    if (data) {
+        notes = JSON.parse(data);
+    }
+
+}
+
+function saveData() {
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+let taskElement = document.getElementById('list-task');
+
+function renderTask() {
+    taskElement.innerHTML = "";
+    notes.forEach((note, index) => {
+        const newElement = `
+            <div class = "note">
+                <h3>${note.title}</h3>
+                <p class = "desc-task">${note.description}<p>
+                <p class = "Deadline-task">Deadline: ${note.deadline}</p>
+                <button onclick="deleteTask(${index})">Hapus</button>
+            </div>
+        `
+        taskElement.innerHTML += newElement;
+    })
+}
+
+function addTask() {
+    const newTitle = document.getElementById('nama-tugas').value;
+    const newDescription = document.getElementById('deskripsi-tugas').value;
+    const newDeadline = document.getElementById('deadline-tugas').value;
+    if (newTitle && newDescription && newDeadline && newTitle.length > 1 && newDescription.length > 1 && newDeadline != null) {
+        const newData = {
+            title: newTitle,
+            description: newDescription,
+            deadline: newDeadline
+        };
+        notes.push(newData);
+        saveData();
+        renderTask();
+    }
+}
+
+function deleteTask(index) {
+    notes.splice(index, 1);
+    saveData();
+    renderTask();
+}
+
+loadData();
+renderTask();
